1386b2de607d6fc7530ebf7cc1675fdd132e95a1
Faebel <132043870+Faebel@users.noreply.github.com>
2024-01-08 10:32:48 +0100

    Nav-Bar update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     Tasks - Projetarbeit Modul 346
                 </a>
 
-                <a class="navbar-brand" href="{{ url('bewertung') }}" style="font-family: Arial Black, sans-serif; margin-left: 105%;">
+                <a class="navbar-brand" href="{{ url('bewertung') }}" style="font-family: Arial Black, sans-serif; margin-left: 50%;">
                     Bewertung
                 </a>
 
